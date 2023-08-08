985047dc84c22b22e6e8589b9d7d521e19cbb47d
Michaela Leštáková <michaela.lestakova@tu-darmstadt.de>
2023-08-08 16:53:21 +0200

    Added deserializer

--- plot_serializer/serializer.py
+++ plot_serializer/serializer.py
@@ -5,11 +5,23 @@ from plot_serializer.adapters import Plot, MatplotlibAdapter
 
 class Serializer:
     def __init__(self, p) -> None:
+        self._plot = None
         self.plot = self.convert_plot(p)
         pass
 
+    @property
+    def plot(self):
+        return self._plot
+
+    @plot.setter
+    def plot(self, plot):
+        if not issubclass(type(plot), Plot):
+            raise TypeError("plot must be a subclass of plot_serializer.adapters.Plot")
+        else:
+            self._plot = plot
+
     def convert_plot(self, p) -> Plot:
-        if type(p) == matplotlib.pyplot.Figure:
+        if isinstance(p, matplotlib.pyplot.Figure):
             converted_plot = MatplotlibAdapter(p)
         else:
             raise NotImplementedError(
@@ -17,7 +29,6 @@ class Serializer:
             )
         return converted_plot
 
-    def to_json(self) -> None:
-        return json.loads(
-            json.dumps(self.plot, default=lambda o: getattr(o, "__dict__", str(o)))
-        )
+    def to_json(self) -> str:
+        return json.dumps(self.plot, default=lambda o: getattr(o, "__dict__", str(o)))
+
