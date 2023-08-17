e8cb2309c1620ec68d963beb8cb5e27328a33f8f
Michaela Leštáková <michaela.lestakova@tu-darmstadt.de>
2023-08-17 16:08:14 +0200

    Added ontology checking

--- plot_serializer/plot.py
+++ plot_serializer/plot.py
@@ -1,3 +1,6 @@
+from plot_serializer.utils import unit_in_ontology
+
+
 class Plot:
     # __slots__ = ["_id", "_axes", "_title", "_caption"]
     def __init__(self) -> None:
@@ -40,6 +43,88 @@ class Axis:
         self._yunit = None
         pass
 
+    @property
+    def plotted_elements(self):
+        return self._plotted_elements
+
+    @plotted_elements.setter
+    def plotted_elements(self, pe):
+        if not all(isinstance(p, PlottedElement) for p in pe):
+            raise TypeError("Must be a list of instances of PlottedElement.")
+        self._plotted_elements = pe
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str):
+            raise TypeError("title must be a string.")
+        self._title = title
+
+    @property
+    def xlabel(self):
+        return self._xlabel
+
+    @xlabel.setter
+    def xlabel(self, xlabel):
+        if not isinstance(xlabel, str):
+            raise TypeError("xlabel must be a string.")
+        self._xlabel = xlabel
+
+    @property
+    def ylabel(self):
+        return self._ylabel
+
+    @ylabel.setter
+    def ylabel(self, ylabel):
+        if not isinstance(ylabel, str):
+            raise TypeError("ylabel must be a string.")
+        self._ylabel = ylabel
+
+    @property
+    def xquantity(self):
+        return self._xquantity
+
+    @xquantity.setter
+    def xquantity(self, xquantity):
+        if not isinstance(xquantity, str):
+            raise TypeError("xquantity must be a string.")
+        self._xquantity = xquantity
+
+    @property
+    def yquantity(self):
+        return self._yquantity
+
+    @yquantity.setter
+    def yquantity(self, yquantity):
+        if not isinstance(yquantity, str):
+            raise TypeError("yquantity must be a string.")
+        self._yquantity = yquantity
+
+    @property
+    def xunit(self):
+        return self._xunit
+
+    @xunit.setter
+    def xunit(self, xunit):
+        if not isinstance(xunit, str):
+            raise TypeError("xunit must be a string.")
+        if not unit_in_ontology(xunit):
+            raise Warning("Unit {xunit} is not in the OM ontology.")
+        self._xunit = xunit
+
+    @property
+    def yunit(self):
+        return self._yunit
+
+    @yunit.setter
+    def yunit(self, yunit):
+        if not isinstance(yunit, str):
+            raise TypeError("yunit must be a string.")
+        self._yunit = yunit
+
 
 class PlottedElement:
     def __init__(self) -> None:
@@ -54,7 +139,7 @@ class PlottedElement:
 
     @xdata.setter
     def xdata(self, data):
-        if type(data) is not list:
+        if not isinstance(data, list):
             raise TypeError("xdata must be a list.")
         if self.ydata is not None and len(data) != len(self.ydata):
             raise RuntimeError("Length of xdata and ydata differs.")
@@ -66,7 +151,7 @@ class PlottedElement:
 
     @ydata.setter
     def ydata(self, data):
-        if type(data) is not list:
+        if not isinstance(data, list):
             raise TypeError("ydata must be a list.")
         if self.xdata is not None and len(data) != len(self.xdata):
             raise RuntimeError("Length of xdata and ydata differs.")
@@ -78,7 +163,7 @@ class PlottedElement:
 
     @label.setter
     def label(self, label):
-        if type(label) is not str:
+        if not isinstance(label, str):
             raise TypeError("label must be a string.")
         self._label = label
 
