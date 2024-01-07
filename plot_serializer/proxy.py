ff678b986b2c3952990ab3c598bab4f36c69ae5a
RodneyMKay <36546810+RodneyMKay@users.noreply.github.com>
2024-01-07 22:41:33 +0100

    Simplify the proxy *even* more

--- plot_serializer/proxy.py
+++ plot_serializer/proxy.py
@@ -8,13 +8,8 @@ class Proxy(Generic[T]):
     def __init__(self, delegate: T) -> None:
         self._delegate = delegate
 
-    def __getattribute__(self, __name: str) -> Any:
-        own_value = super().__getattribute__(__name)
+    def __getattr__(self, __name: str) -> Any:
+        return getattr(self._delegate, __name)
 
-        if own_value is not None:
-            return own_value
-
-        return self._delegate.__getattribute__(__name)
-
-    def proxy_call(self, name: str, *args: Any, **kwargs: Any) -> Any:
-        return self._delegate.__getattribute__(name)(*args, **kwargs)
+    def delegate(self) -> T:
+        return self._delegate
